97c6dab6db6da353aec912166e8732b0896ffb03
chenyj <gd_chenyj163.com>
2024-03-29 22:12:23 +0800

    1. 增加admin页面 2. 增加路由配置

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App), // 展示当前目录下的App.vue
-}).$mount('#app') // 替换public下index里的<div id="app"></div>
+  router,
+  render: h => h(App),
+}).$mount('#app')
